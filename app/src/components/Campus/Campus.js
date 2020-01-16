61168fcb3e897a7f15adfd267d89b88b72d3b83b
Jonathan Guzman <jonguzman5@gmail.com>
2020-01-16 12:24:26 -0500

    add single campus page layout

--- app/src/components/Campus/Campus.js
+++ app/src/components/Campus/Campus.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import '../../css/Campus.css';
 import CampusImage from './CampusImage';
+import CampusAddress from './CampusAddress';
 import CampusName from './CampusName';
 import CampusDesc from './CampusDesc';
 import Student from '../Student/Student';
@@ -12,12 +13,26 @@ class Campus extends Component {
       <div>
         <div className="container">
           <div className="box box1">
-            <CampusImage/>
-            <CampusName/>
-            <CampusDesc/>
+            <div className="item item1">
+              <CampusImage/>
+              <CampusAddress/>
+            </div>
+            <div className="item item2">
+              <CampusName/>
+              <CampusDesc/>
+            </div>
           </div>
           <div className="box box2">
-            {/*<Student/>*/}
+            <Student/>
+            <Student/>
+            <Student/>
+            <Student/>
+            <Student/>
+            <Student/>
+            <Student/>
+            <Student/>
+            <Student/>
+            <Student/>
           </div>
         </div>
       </div>
---
+++ app/src/components/Campus/CampusAddress.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../../css/Campus.css';
+
+function CampusAddress(){
+  //const address = this.props.address;
+  const address = "323 W Adress St Citystate, Provence 78778";
+  return (
+    <p>{address}</p>
+  );
+}
+
+export default CampusAddress;
