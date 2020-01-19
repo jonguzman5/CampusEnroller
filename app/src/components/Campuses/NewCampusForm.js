627794a833cebc7444129e3426649505ff5ca530
esm2000 <embazor@gmail.com>
2020-01-18 23:02:51 -0500

    enable create campus form with functionality

--- app/src/components/Campuses/NewCampusForm.js
+++ app/src/components/Campuses/NewCampusForm.js
@@ -10,18 +10,21 @@ class NewCampusForm extends Component {
     console.log('Adding new school to database...');
   }
 
-  handleChange = (e) => {
+  handleNameChange = (e) => {
     console.log(e.target.value);
-    //this.setState({school: e.target.value});
+    
   }
 
   render(){
     return (
       <div className="newcampusform-container">
-        <form onSubmit={this.handleSubmit}>
+        <form onSubmit={this.handleSubmit} method="POST" action="http://localhost:3000/campuses">
           <h1>New Campus Form</h1>
           <h2>Campus Name</h2>
-          <input type='text' onChange={this.handleChange}/>
+          <input type='text' name="name" onChange={this.handleChange}/>
+          <h2>Campus Address</h2>
+          <input type='text' name="address" onChange={this.handleChange}/>
+          <input className="hide" value="https://www.usnews.com/dims4/USNEWS/0b40ca9/17177859217/resize/800x540%3E/quality/85/?url=https%3A%2F%2Fmedia.beam.usnews.com%2Fed%2F49512dcc50e5394df36dccecb41082%2FUSNews18_MainHall.jpg" type='text' name="imageurl" onChange={this.handleChange}/>
           <input type='submit' value="Add Campus"/> {/*ADD REDIRECT TO /CAMPUSES*/}
         </form>
       </div>
